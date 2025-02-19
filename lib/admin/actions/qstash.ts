34462ec00962696f378bdc865821e6c4a200d2fc
Eric Ndege <ontime.c23@gmail.com>
2025-02-19 23:16:40 +0300

    Changes to be committed: 	new file:   lib/admin/actions/qstash.ts 	modified:   package-lock.json 	modified:   package.json

---
+++ lib/admin/actions/qstash.ts
@@ -0,0 +1,20 @@
+import config from "@/lib/config";
+import { Client } from "@upstash/qstash";
+
+const qstash = new Client({ token: config.env.upstash.qstashToken as string });
+
+async function createSchedule() {
+  try {
+    const response = await qstash.schedules.create({
+      destination: `${config.env.apiProdEndpoint}/api/workflows/rent`,
+      body: JSON.stringify({ message: "Hello from Qstash" }),
+      cron: "* * * * *",
+    });
+
+    console.log("Schedule created:", response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+createSchedule();
