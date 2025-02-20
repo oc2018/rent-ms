2db3688bb49b82f19d6a06f60e854a44314c7abc
Eric Ndege <ontime.c23@gmail.com>
2025-02-20 13:28:50 +0300

    editted webhook

--- lib/admin/actions/qstash.ts
+++ lib/admin/actions/qstash.ts
@@ -6,7 +6,7 @@ const qstash = new Client({ token: config.env.upstash.qstashToken as string });
 export async function createSchedule() {
   try {
     const response = await qstash.schedules.create({
-      destination: `${config.env.apiProdEndpoint}/api/workflows/rent`,
+      destination: `${config.env.apiProdEndpoint}/api/webhook/rent`,
       body: JSON.stringify({ message: "Monthly rent Updated" }),
       cron: "* * * * *",
     });
