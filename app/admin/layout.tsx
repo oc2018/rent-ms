eee2cb3becfcebd09b59447b334b51afcd5d05b6
Eric Ndege <ontime.c23@gmail.com>
2025-02-25 23:37:25 +0300

    modified:   app/admin/layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,7 +4,7 @@ import React, { ReactNode } from "react";
 
 import "@/styles/admin.css";
 import Sidebar from "@/components/admin/Sidebar";
-import Header from "@/components/admin/Header";
+// import Header from "@/components/admin/Header";
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import { eq } from "drizzle-orm";
@@ -27,7 +27,7 @@ const layout = async ({ children }: { children: ReactNode }) => {
     <main className="flex min-h-screen w-full flex-row">
       <Sidebar session={session} />
       <div className="admin-container">
-        <Header session={session} />
+        {/* <Header session={session} /> */}
         {children}
       </div>
     </main>
